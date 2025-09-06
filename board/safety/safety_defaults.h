612317536eccf2f8ad8e12c9c9ea27798921c362
Bryan Gerlach <bryan.r.gerlach@gmail.com>
2025-09-05 20:58:24 -0500

    testing

--- board/safety/safety_defaults.h
+++ board/safety/safety_defaults.h
@@ -98,12 +98,24 @@ static int default_fwd_hook(int bus_num, CAN_FIFOMailBox_TypeDef *to_fwd) {
       acc_obj_dist_1 = dist_raw & 0x7F;
       acc_obj_dist_2 = dist_raw >> 7;
     }
-    // FCA11: Detect AEB, override and forward is_scc_msg
+    // SCC14: Forward radar points to sunnypilot/openpilot
     if (addr == 905) {
       uint16_t lat_raw = (GET_BYTE(to_fwd, 2) | ((GET_BYTE(to_fwd, 3) & 0x01) << 8));
       acc_obj_lat_pos_1 = lat_raw & 0xFF;
       acc_obj_lat_pos_2 = (lat_raw >> 8) & 0x1;
       acc_objstatus = (GET_BYTE(to_fwd, 6) >> 3) & 0x7U;
+    }
+    // FCA11: Detect AEB, override and forward is_scc_msg
+    if (addr == 909) {
+      fca_cmd_act = (GET_BYTE(to_fwd, 2) >> 3) & 0x1U;
+      aeb_cmd_act = GET_BYTE(to_fwd, 2) & 0x1U;
+      int8_t raw_deccmd = GET_BYTE(to_fwd, 6) & 0x7FU;
+      cr_vsm_deccmd_fca11 = raw_deccmd - 127;
+      uint8_t cf_vsm_warn_1 = (GET_BYTE(to_fwd, 6) >> 7) & 0x1U;
+      uint8_t cf_vsm_warn_2 = (GET_BYTE(to_fwd, 7) >> 1) & 0x1U;
+      uint8_t cf_vsm_warn_3 = (GET_BYTE(to_fwd, 7) >> 6) & 0x1U;
+      cf_vsm_warn_fca11 = cf_vsm_warn_1 | cf_vsm_warn_2 | cf_vsm_warn_3;
+      cf_vsm_deccmdact_fca11 = ((GET_BYTE(to_fwd, 4) >> 6) & 0x3U) | ((GET_BYTE(to_fwd, 5) & 0x7FU) << 2);
     }
     escc_id(fca_cmd_act, aeb_cmd_act, cf_vsm_warn_fca11, cf_vsm_warn_scc12, cf_vsm_deccmdact_scc12, cf_vsm_deccmdact_fca11, cr_vsm_deccmd_scc12, cr_vsm_deccmd_fca11, obj_valid, acc_objstatus, acc_obj_lat_pos_1, acc_obj_lat_pos_2, acc_obj_dist_1, acc_obj_dist_2, acc_obj_rel_spd_1, acc_obj_rel_spd_2);
     int block_msg = (block && is_scc_msg);
